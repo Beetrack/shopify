c5e26c9c4cc65da1b34be5b18b8716ad123cad55
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-08 18:29:54 -0300

    add key to secrets

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "96c8a662f3fbd53637e965d748d5c412"
-  config.secret = "07ea40824e5cf24e82aa6db4a857ef7e"
+  config.api_key = Rails.application.secrets.shopify['api_key']
+  config.secret =  Rails.application.secerts.shopify['secret']
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
