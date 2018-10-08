7ec5ea89dd321ca9df1733b584f763d3cb71cb44
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-08 18:43:18 -0300

    change config

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,9 +1,11 @@
-ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
-  config.api_key = Rails.application.secrets.shopify['api_key']
-  config.secret =  Rails.application.secrets.shopify['secret']
-  config.scope = "read_orders, read_products"
-  config.embedded_app = true
-  config.after_authenticate_job = false
-  config.session_repository = Shop
+if Rails.env.production?
+  ShopifyApp.configure do |config|
+    config.application_name = "My Shopify App"
+    config.api_key = Rails.application.secrets.shopify['api_key']
+    config.secret =  Rails.application.secrets.shopify['secret']
+    config.scope = "read_orders, read_products"
+    config.embedded_app = true
+    config.after_authenticate_job = false
+    config.session_repository = Shop
+  end
 end
