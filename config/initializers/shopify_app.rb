168a63cae8567850b3f0d4801fad6a5f5a504378
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-09 09:36:25 -0300

    remove secrets

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,11 +1,9 @@
-if Rails.env.production?
-  ShopifyApp.configure do |config|
-    config.application_name = "My Shopify App"
-    config.api_key = Rails.application.secrets.shopify['api_key']
-    config.secret =  Rails.application.secrets.shopify['secret']
-    config.scope = "read_orders, read_products"
-    config.embedded_app = true
-    config.after_authenticate_job = false
-    config.session_repository = Shop
-  end
+ShopifyApp.configure do |config|
+  config.application_name = "My Shopify App"
+#  config.api_key = Rails.application.secrets.shopify['api_key']
+#  config.secret =  Rails.application.secrets.shopify['secret']
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+  config.after_authenticate_job = false
+  config.session_repository = Shop
 end
