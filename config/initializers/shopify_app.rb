ed170cacabba6037b6efe188d5980225de530d93
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-09 10:45:47 -0300

    change token

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = '96c8a662f3fbd53637e965d748d5c412'
-  config.secret = '07ea40824e5cf24e82aa6db4a857ef7e'
+  config.api_key = "96c8a662f3fbd53637e965d748d5c412"
+  config.secret = "d1c4f62b864cb11491823372fbbd8261"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
