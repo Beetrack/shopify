e7d6a0d4cbba8a6a5ae5869698e0a303cc0d3494
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-16 16:02:01 -0300

    test carrier services

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ShopifyApp::AuthenticatedController
     @shop = ShopifyAPI::Shop.current
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
+    @carrier_services = ShopifyAPI::CarrierService.find(:all)
   end
 end
