03ba5880e0754982f2c294623a8f3c006776fdd0
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-16 15:48:33 -0300

    test current shop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
+    @shop = ShopifyAPI::Shop.current
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
