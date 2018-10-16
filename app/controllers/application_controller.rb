459d0b0c8eba62d3f22ff2cbb966acfa97d2020c
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-16 18:41:01 -0300

    X-Frame-Options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  after_action :allow_shopify_iframe
   protect_from_forgery with: :exception
+
+  private
+
+  def allow_shopify_iframe
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
+  end
+
 end
