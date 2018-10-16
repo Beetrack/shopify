282e46cfb7a1cd5fbd7d871e50f3bc3bb7371213
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-16 16:16:47 -0300

    rescue call carrier services

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ShopifyApp::AuthenticatedController
     @shop = ShopifyAPI::Shop.current
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
-    @carrier_services = ShopifyAPI::CarrierService.find(:all)
+    begin
+      @carrier_services = ShopifyAPI::CarrierService.find(:all)
+    rescue => e
+      puts e
+    end
   end
 end
