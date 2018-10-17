a373a5ba6381f0cca9b96b92e7e294713c5fb017
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-17 10:55:47 -0300

    view new carrier service

--- app/controllers/carrier_services_controller.rb
+++ app/controllers/carrier_services_controller.rb
@@ -10,9 +10,19 @@ class CarrierServicesController < ShopifyApp::AuthenticatedController
     end
   end
 
+  def new; end
+
   def edit; end
 
-  def create; end
+  def create
+    logger.info "Params Carrier Service: #{params_carrier_service}"
+  end
 
   def delete; end
+
+  private
+
+  def params_carrier_service
+    params.permit(:name, :callback_url, :service_dicovery)
+  end
 end
