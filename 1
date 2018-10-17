b79cfdce83808aa6cbf7f3a03bd90a009ad16b38
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-17 11:55:35 -0300

    controller test

---
+++ 1
@@ -0,0 +1,5 @@
+class TestController < ApplicationController::Base
+  def test
+    logger.debug "Params Test #{params.to_json}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :carrier_services, except: [:show]
+
+  post 'test' => 'test#test'
+
 end
