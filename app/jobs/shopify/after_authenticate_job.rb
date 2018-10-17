d64b1caffeecdef5377aa5a634bb259f2ebfa3fb
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-17 15:43:29 -0300

    create callback on install app

--- app/jobs/shopify/after_authenticate_job.rb
+++ app/jobs/shopify/after_authenticate_job.rb
@@ -4,7 +4,12 @@ module Shopify
       shop = Shop.find_by(shopify_domain: shop_domain)
 
       shop.with_shopify_session do
-        logger.info "TEST AFTER AUTHENTICATE JOB"
+        logger.info 'TEST AFTER AUTHENTICATE JOB'
+        ShopifyAPI::CarrierService.create(
+          name: 'Carrier Service Test',
+          callback_url: 'https://daniel-shopify-test.herokuapp.com/test',
+          service_discovery: true
+        )
       end
     end
   end
