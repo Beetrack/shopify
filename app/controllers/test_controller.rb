de65e1b9f26014de092f8a001b5e77096a9ac8e6
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-17 12:11:33 -0300

    response callback

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,8 +1,20 @@
 class TestController < ActionController::Base
   def test
     logger.debug "Params Test #{params.to_json}"
-    render json: {
-      status: :ok
+    render json: response_callback
+  end
+
+  private
+
+  def response_callback
+    {
+      service_name: 'canadapost-overnight',
+      service_code: 'ON',
+      total_price: '1295',
+      description: 'This is the fastest option by far',
+      currency: 'CAD',
+      min_delivery_date: '2013-04-12 14:48:45 -0400',
+      max_delivery_date: '2013-04-12 14:48:45 -0400'
     }
   end
 end
