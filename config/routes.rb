e3f3f9da21721419a8c6d84ad26bd58a57a10938
Daniel Mella Huerta <dmella04@gmail.com>
2018-10-18 18:30:07 -0300

    remove testing controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :carrier_services, except: [:show]
-
-  post 'test' => 'test#test'
 
 end
